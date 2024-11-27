0f267e5030e6663a73dc6e711a2946ff18807d2d
soletsdev <soletsdev@github>
2024-11-27 11:09:01 -0800

    adding secret and public values

--- tools/config/script.js
+++ tools/config/script.js
@@ -1,10 +1,15 @@
 // @ts-check
 /** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
+
 module.exports = ({ github, context, process }) => {
   console.log(`SHA: ${context.sha}`);
   console.log(`REF: ${context.ref}`);
   console.log("environment variables");
-  console.log(process);
+  console.log(process.env.REALM_ID);
+  console.log(process.env.KEYCLOAK_URL);
+  console.log(process.env.SECRET_VALUE);
+  console.log(process.env.PUBLIC_VALUE);
+
   console.log("=======================");
   console.log(process.env);
 };
