9b988e07198bbeea133cdbac2a6bd956d25fc9be
soletsdev <soletsdev@github>
2024-11-27 12:11:00 -0800

    trying to fail job

--- tools/config/script.js
+++ tools/config/script.js
@@ -1,7 +1,7 @@
 // @ts-check
 /** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
 
-module.exports = ({ github, context, process }) => {
+module.exports = ({ github, context, process, core }) => {
   console.log(`SHA: ${context.sha}`);
   console.log(`REF: ${context.ref}`);
   console.log("environment variables");
@@ -12,17 +12,11 @@ module.exports = ({ github, context, process }) => {
 
   console.log("TESTING JSON PARSING");
   try {
-    console.log("========================== stringify");
-    const myJson = JSON.parse(process.env.SECRET_JSON);
-    console.log(myJson);
-
-    console.log(myJson.test);
-    console.log(JSON.parse(JSON.stringify(process.env.INVALID_SECRET_JSON)));
-
-    console.log("========================== error");
-    console.log(JSON.parse(process.env.INVALID_SECRET_JSON));
+    //ensure the json value is valid before proceeding
+    JSON.parse(process.env.SECRET_JSON);
+    JSON.parse(process.env.INVALID_SECRET);
   } catch (e) {
     console.log("error parsing JSON");
-    console.log(e);
+    core.setFailed("script.js failed");
   }
 };
