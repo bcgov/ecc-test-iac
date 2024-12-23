1978949bdc98ad5426658a6aa11a0543348b68e0
soletsdev <soletsdev@github>
2024-12-23 15:37:36 -0800

    testing pinging keycloak instance

--- tools/config/script.js
+++ tools/config/script.js
@@ -2,16 +2,18 @@ const axios = require("axios");
 
 // @ts-check
 /** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
-module.exports = async ({ github, context, core, process }) => {
+module.exports = async ({ _github, context, core, process }) => {
   console.log(`SHA: ${context.sha}`);
   console.log(`REF: ${context.ref}`);
   console.log("environment variables");
   console.log(process.env.REALM_ID);
-  console.log(process.env.KEYCLOAK_URL);
   console.log(process.env.SECRET_VALUE);
   console.log(process.env.PUBLIC_VALUE);
 
-  const res = await axios.get("https://www.google.com");
+  const KEYCLOAK_URL = `https://${process.env.ENVIRONMENT}.loginproxy.gov.bc.ca/auth/realms/${process.env.REALM_ID}`;
+  console.log(KEYCLOAK_URL);
+
+  const res = await axios.get(KEYCLOAK_URL);
   console.log(res);
 
   console.log("TESTING JSON PARSING");
